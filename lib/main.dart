460c53c23318987fb6e95406812dd56f15aed632
masami <kei11nishikori16@gmail.com>
2020-04-11 12:36:49 +0900

    アニメーションの追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,65 @@
+import 'package:flutter/animation.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+//void main() => runApp(MyApp());
 
+void main() => runApp(LogoApp());
+
+
+class LogoApp extends StatefulWidget {
+  _LogoAppState createState() => _LogoAppState();
+}
+
+class _LogoAppState extends State<LogoApp> with SingleTickerProviderStateMixin {
+
+  Animation<double> animation;
+  AnimationController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller =
+        AnimationController(duration: const Duration(seconds: 2), vsync: this);
+    animation = Tween<double>(begin: 0, end: 300).animate(controller)
+    ..addStatusListener((status) {
+      if(status == AnimationStatus.completed ) {
+        controller.reverse();
+      } else if(status == AnimationStatus.dismissed) {
+        controller.forward();
+      }
+    })
+    ..addStatusListener((status) => print('$status') );
+    controller.forward();
+  }
+
+  @override
+  Widget build(BuildContext context) => AnimatedLogo(animation: animation);
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+}
+
+class AnimatedLogo extends AnimatedWidget {
+  AnimatedLogo({Key key, Animation<double> animation})
+      : super(key: key, listenable: animation);
+
+  Widget build(BuildContext context) {
+    final animation = listenable as Animation<double>;
+    return Center(
+      child: Container(
+        margin: EdgeInsets.symmetric(vertical: 10),
+        height: animation.value,
+        width: animation.value,
+        child: FlutterLogo(),
+      )
+    );
+  }
+}
+
+/*
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
@@ -200,3 +258,4 @@ class _FavoriteWidgetState extends State<FavoriteWidget> {
     );
   }
 }
+*/
